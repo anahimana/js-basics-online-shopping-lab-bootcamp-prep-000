33b0a5b2a7892327fefc55e97ad2fad63e491c1a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   // Empty the cart array
+  console.log("Hello!");
   console.log(setCart());
 }
 
