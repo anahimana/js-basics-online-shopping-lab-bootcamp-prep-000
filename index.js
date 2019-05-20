dffcca365ad18204ff55aaadf37ac6fbf7c3769a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:49:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }else {
     const message = `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`;
-    setCart(c);
+    setCart([]);
     return 
   }
 }
