6f4ff80d4e9802e4b900a23082b599fb0f606e89
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:46:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   }else {
-    
+    return `Your total cost is \$${tota()}, which will be charged to the card 83296759.`;
   }
 }
 
