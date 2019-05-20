94128c21d0d6d134b2ea91592bfa8f7db05c8c07
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 11:52:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function removeFromCart(item) {
     // console.log("It is not there!");
     return "That item is not in your cart.";
   }
-  
+  return "Leaving the function!"
 }
 
 console.log(removeFromCart('Oranges'));
