5033f30a867c9753abc1262813b8c24b69113100
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:16:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   const removeCart = getCart();
-  console.log(removeCart)
+  console.log(removeCart[0])
   for(let i = 0; i < removeCart.length; i++) {
     // console.log(removeCart)
     // console.log(removeCart[i].itemName)
