b648ce9d8fef010db2429914e3cb8223e18c963a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
       console.log("Found item!");
       removeCart.splice(i, 1);
     }else {
-      return ""
+      return "That item is not in your cart."
     }
   }
 }
