956892cf173c9054dce6e6f84bbc5f6a7102e15c
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -97,8 +97,9 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   }else {
-    const message = 
-    return `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`;
+    const message = `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`;
+    setCart(cart)
+    return 
   }
 }
 
