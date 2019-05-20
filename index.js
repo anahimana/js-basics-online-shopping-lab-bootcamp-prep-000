5f03c754ea421ce934b1a3eb17b68034fc8d2a39
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:13:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,6 @@ function removeFromCart(item) {
   // write your code here
   const removeCart = getCart();
   for(let i = 0; i < removeCart.length; i++) {
-      console.log("You range!")
     if (removeCart[i].itemName === item) {
       console.log("Found item!");
       removeCart.splice(i, 1);
