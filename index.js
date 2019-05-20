4acb07b8bbda225d5def74d851f87224cd3ca137
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-20 13:38:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber === undefined) {
-    return ""
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
 
